eda61fbaea3e531464d33dea7ce1b6d584b27462
Md. A. Barik <48405620+mdabarik@users.noreply.github.com>
2023-05-07 08:41:53 +0600

    Create 58--palindrome-substrings.java

---
+++ 58--palindrome-substrings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            count += find(s, i, i);
+            count += find(s, i, i + 1);
+        }
+        return count;
+    }
+    public int find(String s, int left, int right) {
+        int count = 0;
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            count += 1;
+            left--;F
+            right++;
+        }
+        return count;
+    }
+} // TC: O(n^2), SC: O(1)
