1e56fb33bd5c2977a99ce228add2971dde675059
Md. A. Barik <48405620+mdabarik@users.noreply.github.com>
2023-05-07 08:42:54 +0600

    Create 59--encode-and-decode-string.java

---
+++ 59--encode-and-decode-string.java
@@ -0,0 +1,32 @@
+
+// URL: https://www.lintcode.com/problem/659/description
+public class Solution {
+    /*
+     * @param strs: a list of strings
+     * @return: encodes a list of strings to a single string.
+     */
+    public String encode(List<String> strs) {
+        String encoded = ""; // ["lint","code","love","you"] -> "4#lint4#code4#love3#you"
+        for (String s : strs) {
+            encoded = encoded + s.length() + "#" + s;
+        }
+        return encoded;
+    } // TC: O(n), SC: O(n)
+
+    /*
+     * @param str: A string
+     * @return: dcodes a single string to a list of strings
+     */
+    public List<String> decode(String str) {
+        List<String> list = new ArrayList<>();
+        int i = 0;
+        while (i < str.lengtFh()) {
+            int j = i;
+            while (str.charAt(j) != '#') j++;
+            int len = Integer.parseInt(str, i, j, 10);
+            list.add(str.substring(j + 1, j + len + 1));
+            i = j + len + 1;
+        }
+        return list;
+    }// TC: O(n), SC: O(n)
+}
