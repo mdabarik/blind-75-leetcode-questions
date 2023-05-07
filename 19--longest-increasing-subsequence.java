a74f3dc843184c1ad44653f302402f50d4501f5c
Md. A. Barik <48405620+mdabarik@users.noreply.github.com>
2023-05-07 08:10:11 +0600

    Create 19--longest-increasing-subsequence.java

---
+++ 19--longest-increasing-subsequence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+                }
+            }
+        }
+        return dp[text1.length()][text2.length()];
+    }
+} // TC: O(m * n), SC: O(m * n)
