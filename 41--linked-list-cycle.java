0d5c9022828ee8aeeec95748d97a26dde0d7f6f2
Md. A. Barik <48405620+mdabarik@users.noreply.github.com>
2023-05-07 08:29:59 +0600

    Create 41--linked-list-cycle.java

---
+++ 41--linked-list-cycle.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) return false;
+        ListNode slow = head, fast = head; 
+        while (fast != null && fast.next != null) {
+            if (fast == null || fast.next == null) return false;
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return true;
+    }
+} // TC: O(n), SC: O(1)
