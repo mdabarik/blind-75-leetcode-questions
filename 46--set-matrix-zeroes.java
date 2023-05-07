54c0b8fd3a770b527b061b4a8b37874696584724
Md. A. Barik <48405620+mdabarik@users.noreply.github.com>
2023-05-07 08:35:07 +0600

    Create 46--set-matrix-zeroes.java

---
+++ 46--set-matrix-zeroes.java
@@ -0,0 +1,49 @@
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean hasFirstRow = false;
+        boolean hasFirstCol = false;
+        for (int c = 0; c < matrix[0].length; c++) { // first row
+            if (matrix[0][c] == 0) {
+                hasFirstRow = true;
+                break;
+            }
+        }
+        for (int r = 0; r < matrix.length; r++) { // first column
+            if (matrix[r][0] == 0) {
+                hasFirstCol = true;
+                break;
+            }F
+        }
+        for (int r = 1; r < matrix.length; r++) { // preprocessing
+            for (int c = 1; c < matrix[0].length; c++) {
+                if (matrix[r][c] == 0) {
+                    matrix[r][0] = 0;
+                    matrix[0][c] = 0;
+                }
+            }
+        }
+        for (int c = 1; c < matrix[0].length; c++) { // 1st row
+            if (matrix[0][c] == 0) {
+                nullifyCol(matrix, c);
+            }
+        }
+        for (int r = 1; r < matrix.length; r++) { // first column
+            if (matrix[r][0] == 0) {
+                nullifyRow(matrix, r);
+            }
+        }
+        if (hasFirstRow) nullifyRow(matrix, 0);
+        if (hasFirstCol) nullifyCol(matrix, 0);
+    }
+    public void nullifyRow(int[][] matrix, int row) {
+        for (int c = 0; c < matrix[0].length; c++) {
+            matrix[row][c] = 0;
+        }
+    }
+    public void nullifyCol(int[][] matrix, int col) {
+        for (int r = 0; r < matrix.length; r++) {
+            matrix[r][col] = 0;
+        }
+    }
+} // TC: O(R) + O(C) + O(R * C) + O(R * C), SC: O(1)
