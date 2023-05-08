0788535ef69670eca51ed893b9c9fb210ed19e86
Md. A. Barik <48405620+mdabarik@users.noreply.github.com>
2023-05-08 14:08:59 +0600

    Create 32--alien-dictionary.java

---
+++ 32--alien-dictionary.java
@@ -0,0 +1,58 @@
+class Solution {
+    public String alienOrder(String[] words) {
+        Map<Character, Set<Character>> graph = new HashMap<>();
+        int[] inDegree = new int[26];
+        buildGraph(words, graph, inDegree);
+        
+        String order = topologicalSort(graph, inDegree);
+        return order.length() == graph.size() ? order : "";
+    }
+    
+    private void buildGraph(String[] words, Map<Character, Set<Character>> graph, int[] inDegree) {
+        for (String word : words) {
+            for (char c : word.toCharArray()) {
+                graph.put(c, new HashSet<>());
+            }
+        }
+        
+        for (int i = 1; i < words.length; i++) {
+            String first = words[i - 1];
+            String second = words[i];
+            int length = Math.min(first.length(), second.length());
+            
+            for (int j = 0; j < length; j++) {
+                char parent = first.charAt(j);
+                char child = second.charAt(j);
+                if (parent != child) {
+                    if (!graph.get(parent).contains(child)) {
+                        graph.get(parent).add(child);
+                        inDegree[child - 'a']++;
+                    }
+                    break;
+                }
+            }
+        }
+    }
+    
+    private String topologicalSort(Map<Character, Set<Character>> graph, int[] inDegree) {
+        Queue<Character> queue = new LinkedList<>();
+        for (char c : graph.keySet()) {
+            if (inDegree[c - 'a'] == 0) {
+                queue.offer(c);
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        while (!queue.isEmpty()) {
+            char c = queue.poll();
+            sb.append(c);
+            for (char neighbor : graph.get(c)) {
+                inDegree[neighbor - 'a']--;
+                if (inDegree[neighbor - 'a'] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
