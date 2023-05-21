7b40dc65de4f2d1dae7630dbef5d7963c3dc94e1
Md. A. Barik <48405620+mdabarik@users.noreply.github.com>
2023-05-21 17:44:33 +0600

    Update 01--two-sum.java

--- 01--two-sum.java
+++ 01--two-sum.java
@@ -2,14 +2,13 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] ans = new int[2];
         HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) { // n
-            int diff = target - nums[i];
-            if (map.containsKey(diff)) {
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i]; // [nums[i], diff]
+            if (!map.containsKey(diff)) {
+                map.put(nums[i], i);
+            } else {
                 ans[0] = i;
                 ans[1] = map.get(diff);
-                break;
-            } else {
-                map.put(nums[i], i);
             }
         }
         return ans;
