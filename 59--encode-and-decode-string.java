922b7a1d93fc7e56c225a35f07487beb685670cc
Md. A. Barik <48405620+mdabarik@users.noreply.github.com>
2023-08-06 07:11:12 +0600

    Update 59--encode-and-decode-string.java

--- 59--encode-and-decode-string.java
+++ 59--encode-and-decode-string.java
@@ -1,6 +1,18 @@
 /*
 Design an algorithm to encode a list of strings to a string. 
 The encoded string is then sent over the network and is decoded back to the original list of strings.
+
+Example 1:
+Input: ["lint","code","love","you"]
+Output: ["lint","code","love","you"]
+Explanation:
+One possible encode method is: "lint:;code:;love:;you"
+
+Example 2:
+Input: ["we", "say", ":", "yes"]
+Output: ["we", "say", ":", "yes"]
+Explanation:
+One possible encode method is: "we:;say:;:::;yes"
 */
 
 /*
